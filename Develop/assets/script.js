9a744b8bfd6642629358fa934689cf49ba5b05c6
Anhkhoa Nguyen <anhvudnguyen27@gmail.com>
2022-08-15 21:37:19 -0700

    button changes

---
+++ Develop/assets/script.js
@@ -0,0 +1,11 @@
+// textarea
+
+var locationText = document.getElementById('locationText');
+var foodText = document.getElementById('foodText');
+var bringText = document.getElementById('bringText');
+
+
+
+
+// button functions
+
