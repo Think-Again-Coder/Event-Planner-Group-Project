8b3ca0e398fceccf4143602a98e496877d670ee3
unknown <lopez.thinktank@gmail.com>
2022-08-15 21:14:37 -0700

    updated the Heading and added a calendar using bulma js files

---
+++ Develop/assets/script.js
@@ -0,0 +1,19 @@
+//calendar date 
+const calendars = bulmaCalendar.attach('[type="date"]', options);
+
+// Loop on each calendar initialized
+calendars.forEach(calendar => {
+	// Add listener to select event
+	calendar.on('select', date => {
+		console.log(date);
+	});
+});
+
+// To access to bulmaCalendar instance of an element
+const element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', datepicker => {
+		console.log(datepicker.data.value());
+	});
+}
